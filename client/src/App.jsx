a775df467a05bd719f78b2e9f38c01598c85ca69
Anwar_Hossain <ranakrphone@gmail.com>
2026-07-12 14:50:42 +0600

    tan stack setup done in client part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,35 @@ import { router } from "./router/router";
 import { Toaster } from "react-hot-toast";
 import { LanguageProvider } from "./providers/LanguageProvider";
 import { ThemeProvider } from "./providers/ThemeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 const App = () => {
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
   return (
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <AuthProvider>
-        <LanguageProvider>
-          <ThemeProvider>
-            <RouterProvider router={router} />
-
-            <Toaster position="top-center" reverseOrder={false} />
-          </ThemeProvider>
-        </LanguageProvider>
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <AuthProvider>
+          <LanguageProvider>
+            <ThemeProvider>
+              <RouterProvider router={router} />
+              <Toaster position="top-center" reverseOrder={false} />
+            </ThemeProvider>
+          </LanguageProvider>
+        </AuthProvider>
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   );
 };
 
